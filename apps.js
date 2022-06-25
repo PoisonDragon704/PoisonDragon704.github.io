9f6242043462672c2e2521206d2acc898624b59f
AppInstaller iOS <benjaminhornbeck6@icloud.com>
2022-06-25 14:49:34 -0400

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,22 @@
+var p12 = "0ae3d6266637"
+var mobileprovision = "4d78b728b109"
+var password = "1"
+
+function unc0ver() {
+window.open("https://sign.starfiles.co/?ipa=1bca09a52a24&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function Taurine() {
+window.open("https://sign.starfiles.co/?ipa=3608d1660667&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function Odyssey() {
+window.open("https://sign.starfiles.co/?ipa=e4070f&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function Chimera() {
+window.open("https://sign.starfiles.co/?ipa=c6078ec402&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function FilzaEscaped15() {
+window.open("https://sign.starfiles.co/?ipa=c6078ec402&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function Delta() {
+window.open("https://sign.starfiles.co/?ipa=tNnsA9OPlh07&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
