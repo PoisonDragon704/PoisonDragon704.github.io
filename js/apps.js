d6cbe0b6dfb3c3f8d56172b6c980e1fb2f5c4d62
Dragon <92691789+PoisonDragon704@users.noreply.github.com>
2022-07-08 10:16:12 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,5 +1,5 @@
-var p12 = "ad9905ec941a"
-var mobileprovision = "b530d8f4ac5d"
+var p12 = "468de2e8ef10"
+var mobileprovision = "b38982c29149"
 var password = "1qaz@wsx3edc"
 
 function unc0ver() {
@@ -21,16 +21,16 @@ function Delta() {
 window.open("https://sign.starfiles.co/?ipa=5c8205d85428&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
 }
 function iPogo() {
-window.open("https://sign.starfiles.co/?ipa=fedc214b85b7&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+window.open("https://sign.starfiles.co/?ipa=3f99e22ef11b&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
 }
 function INDS() {
-window.open("https://sign.starfiles.co/?ipa=ef45fd2425ca&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+window.open("https://sign.starfiles.co/?ipa=51b8b6fe21c5&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
 }
 function ZiniTevi() {
-window.open("https://sign.starfiles.co/?ipa=08f2012ac6e1&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+window.open("https://sign.starfiles.co/?ipa=afe21214be31&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
 }
 function PPSSPP() {
-window.open("https://sign.starfiles.co/?ipa=d819eb51ccf3&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+window.open("https://sign.starfiles.co/?ipa=e7be786df532&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
 }
 function SpooferX() {
-window.open("https://sign.starfiles.co/?ipa=d3ec34949fd0&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+window.open("https://sign.starfiles.co/?ipa=da10d60f8be3&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
