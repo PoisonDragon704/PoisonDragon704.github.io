c9a4da8c5265d6bd3168160953449207379dbecd
Dragon <92691789+PoisonDragon704@users.noreply.github.com>
2022-07-20 23:15:15 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -38,4 +38,7 @@ window.open("https://sign.starfiles.co/?ipa=da10d60f8be3&p12=" + p12 + "&mobilep
 function Root() {
 window.open("https://sign.starfiles.co/?ipa=104a124769d3&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
 }
-
+function sign() {
+ipa = document.getElementById("ipa").value;                         
+window.open("https://sign.starfiles.co/?ipa=" + ipa + "&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
